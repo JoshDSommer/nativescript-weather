b26071bde27813e94e4e17b45f1a90c2b9969718
Josh Sommer <JoshDSommer@gmail.com>
2016-10-11 21:39:29 -0400

    merge conflicts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,47 +1,15 @@
-<<<<<<< HEAD
-import {Component, ViewChild, ViewEncapsulation, ElementRef, AfterViewInit, ChangeDetectorRef  } from "@angular/core";
-import {NgIf } from '@angular/common';
-import {RouterConfig} from "@angular/router";
-import {topmost} from 'ui/frame';
-import {SwissArmyKnife} from 'nativescript-swiss-army-knife/nativescript-swiss-army-knife';
-import {Page} from 'ui/page';
-import {TNSFontIconService, TNSFontIconPipe} from 'nativescript-ng2-fonticon';
-import {IForecastCardInfo, ForecastIOService, IForecast } from './services/forecast.io.services';
-import {ILocationInfo, LocationService} from './services/location.service';
-import {NS_ROUTER_DIRECTIVES, nsProvideRouter} from 'nativescript-angular/router';
-import * as app from 'application';
-import {Color} from 'color';
-import * as Platform from 'platform';
 
-=======
 import { Component } from "@angular/core";
 import { ForecastIOService, LocationService, ILocationInfo, PageDimensions, PositioningService } from './services';
 import { SwissArmyKnife } from 'nativescript-swiss-army-knife';
 import { TNSFontIconService } from 'nativescript-ng2-fonticon';
->>>>>>> 2
 declare const android: any;
 
 @Component({
-<<<<<<< HEAD
-	selector: 'weather-app',
-	template: `
-		<page-router-outlet></page-router-outlet>
-	`,
-	styles: [
-		`
-			#constainter-wrapper{
-				padding-top:20;
-			}
-		`
-	],
 
-	providers: [ForecastIOService, LocationService]
-=======
     selector: "weather-app",
     templateUrl: "app.component.html",
     providers: [ForecastIOService, LocationService, PageDimensions, PositioningService]
-
->>>>>>> 2
 })
 export class WeatherAppComponent {
     public cityTemp: string;
@@ -66,16 +34,3 @@ export class WeatherAppComponent {
         SwissArmyKnife.setAndroidStatusBarColor('#8ba192');
     }
 }
-<<<<<<< HEAD
-
-var routes: RouterConfig = [
-	{ path: "", component: ForecastComponent },
-	{ path: "location", component: LocationsComponent },
-	{ path: "network", component: NetworkIssueComponent },
-];
-
-export var APP_ROUTES = [
-	nsProvideRouter(routes, { enableTracing: false })
-]
-=======
->>>>>>> 2
